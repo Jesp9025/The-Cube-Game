9431bbfcd1da4400202f72c8c53db6e5cd0568f1
erki0013 <56446844+erki0013@users.noreply.github.com>
2019-10-11 23:24:54 +0200

    Update SubnetCalculator.py

--- SubnetCalculator/SubnetCalculator.py
+++ SubnetCalculator/SubnetCalculator.py
@@ -1,1 +1,23 @@
+import ipaddress
+
+ip=input ("enter ip address: ")
+mask=input ("Enter network mask: ")
+ipm=ip + "/" + mask
+
+hostid = ip[-3:]                                #gets the host id
+hostid = hostid.replace(".", "")                #takes the potential "." out of host id
+net1 = ipaddress.ip_interface(ipm)              #first possible host (host1.network)
+subs = ipaddress.ip_network(net1.network)       #Subnet Number of Hosts (net4.num_addresses)
+floor = int(hostid) / int(subs.num_addresses)   #a number to calculate other things, have to turn them both to integrers
+netid = round(floor) * subs.num_addresses       #calculates the network id
+broadid = netid + (subs.num_addresses - 1)      #calculates broadcast id
+host1 = netid + 1                               #calculates the first usable host
+host2 = broadid - 1                             #calculates the last usable host
+
+print("The network id is:",net1.network)
+#print("The number of subnet hosts is:",subs.num_addresses)
+#print("The network id is:",netid)
+print("The broadcast id is:",broadid)
+print("The first usable host is:",host1)
+print("The last usable host is:",host2)
 
