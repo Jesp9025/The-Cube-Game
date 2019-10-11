a1e4e9eeddd1bc5157856ba5199a080fd24e4ae5
erki0013 <56446844+erki0013@users.noreply.github.com>
2019-10-11 18:52:26 +0200

    Update MathGame.py

--- MathGame/MathGame.py
+++ MathGame/MathGame.py
@@ -1,14 +1,14 @@
 import random
 import time
 
-ops = ['+', '-', '*']
-rand=random.randint(1,100)
-rand2=random.randint(1,100)
-operation = random.choice(ops)
-maths = eval(str(rand) + operation + str(rand2))
-print ("Your question is",rand,operation,rand2)
-d=int(input ("What is your answer:"))
-if d==maths:
-    print ("Correct, you may move to the next room")
+ops = ['+', '-', '*']                                   #adds the options for the equations
+rand=random.randint(1,100)                              #chooses a number between 1 and 100
+rand2=random.randint(1,100)                             #chooses a number between 1 and 100
+operation = random.choice(ops)                          #chooses a random equation from the line before
+maths = eval(str(rand) + operation + str(rand2))        #does the actual equation
+print ("Your question is",rand,operation,rand2)         #tells the user what the equation is
+d=int(input ("What is your answer:"))                   #asks the user for the awnser
+if d==maths:                                            #checks if the input is the same as the awnser
+    print ("Correct, you may move to the next room")    #output if the awnser is correct
 else:
-    print ("Incorrect. The actual answer is",maths)
+    print ("Incorrect. The actual answer is",maths)     #output if the awnser is wrong, and tells the right awnser
