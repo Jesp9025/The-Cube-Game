cfb5ddb2c21a9dcf0c6fa6b58b073c718d6c4693
Jesper Pedersen <54931767+Jesp9025@users.noreply.github.com>
2019-10-11 14:35:32 +0200

    Update GuessNumberGame.py

--- GuessNumberGame.py
+++ GuessNumberGame.py
@@ -11,15 +11,18 @@ while True:
         attempts -= 1
         answer = int(input("Guess the number: "))
         if answer <= 0:
-            print("Can't be less than 1. We won't count this as an attempt")
+            print("Can't be less than 1. We won't count this as an attempt.")
             attempts += 1
             continue
+        elif answer >= 11:
+            print("Can't be higher than 10. We won't count this as an attempt.")
+            attempts += 1
         elif answer == number:
             print("You guessed the correct number!\nYou get 1 point.")
             totalPoints += 1
             print("Total points: ", totalPoints)
             break
-        elif attempts == 0:
+        elif attempts <= 0:
             print("You ran out of attempts.\nYou lose 1 point.")
             totalPoints -= 1
             print("Total points: ", totalPoints)
@@ -29,6 +32,6 @@ while True:
         elif answer < number:
             print("Higher")
     except ValueError:
-        print("That was not a number. We won't count this as an attempt")
+        print("That was not a number. We won't count this as an attempt.")
         attempts += 1
         continue
