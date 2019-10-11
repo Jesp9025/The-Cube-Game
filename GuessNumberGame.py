b90014e2d1f5fc717b2662e279a5be3091f130d7
Jesper Pedersen <54931767+Jesp9025@users.noreply.github.com>
2019-10-11 14:44:16 +0200

    Update GuessNumberGame.py

--- GuessNumberGame.py
+++ GuessNumberGame.py
@@ -8,30 +8,28 @@ print("Welcome to the Number-Guessing-Game\nYou have 3 attempts to guess the cor
 print("########################################")
 while True:
     try:
-        attempts -= 1
         answer = int(input("Guess the number: "))
         if answer <= 0:
             print("Can't be less than 1. We won't count this as an attempt.")
-            attempts += 1
             continue
         elif answer >= 11:
             print("Can't be higher than 10. We won't count this as an attempt.")
-            attempts += 1
-        elif answer == number:
-            print("You guessed the correct number!\nYou get 1 point.")
-            totalPoints += 1
-            print("Total points: ", totalPoints)
-            break
-        elif attempts <= 0:
-            print("You ran out of attempts.\nYou lose 1 point.")
-            totalPoints -= 1
-            print("Total points: ", totalPoints)
-            break
-        elif answer > number:
-            print("Lower")
-        elif answer < number:
-            print("Higher")
+        elif answer >= 1 or answer <= 10:
+            attempts -= 1
+            if answer == number:
+                print("You guessed the correct number!\nYou get 1 point.")
+                totalPoints += 1
+                print("Total points: ", totalPoints)
+                break
+            elif attempts <= 0:
+                print("You ran out of attempts.\nYou lose 1 point.")
+                totalPoints -= 1
+                print("Total points: ", totalPoints)
+                break
+            elif answer > number:
+                print("Try a lower number")
+            elif answer < number:
+                print("Try a higher number")
     except ValueError:
         print("That was not a number. We won't count this as an attempt.")
-        attempts += 1
         continue
