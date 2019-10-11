7567f290bf2e4cc922f9fb99a3007056fcc8b375
erki0013 <56446844+erki0013@users.noreply.github.com>
2019-10-11 18:47:49 +0200

    Update MathGame.py

--- MathGame/MathGame.py
+++ MathGame/MathGame.py
@@ -1,1 +1,14 @@
+import random
+import time
 
+ops = ['+', '-', '*']
+rand=random.randint(1,100)
+rand2=random.randint(1,100)
+operation = random.choice(ops)
+maths = eval(str(rand) + operation + str(rand2))
+print ("Your question is",rand,operation,rand2)
+d=int(input ("What is your answer:"))
+if d==maths:
+    print ("Correct, you may move to the next room")
+else:
+    print ("Incorrect. The actual answer is",maths)
