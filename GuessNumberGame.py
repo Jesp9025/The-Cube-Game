388b989962b713cda0219571eec2888da4f84282
Jesper Pedersen <54931767+Jesp9025@users.noreply.github.com>
2019-10-11 14:14:12 +0200

    Update GuessNumberGame.py

--- GuessNumberGame.py
+++ GuessNumberGame.py
@@ -1,1 +1,27 @@
+import random
 
+totalPoints = 0
+number = random.randint(1,10)
+attempts = 3
+
+print("########################################")
+print("Welcome to the Number-Guessing-Game\nYou have 3 attempts to guess the correct number.\nThe number is generated randomly from 1 to 10")
+print("########################################")
+
+while True:
+    attempts -= 1
+    answer = int(input("Guess the number: "))
+    if answer == number:
+        print("You guessed the correct number!\nYou get 1 point.")
+        totalPoints += 1
+        print("Total points: ", totalPoints)
+        break
+    elif attempts == 0:
+        print("You ran out of attempts.\nYou lose 1 point.")
+        totalPoints -= 1
+        print("Total points: ", totalPoints)
+        break
+    elif answer > number:
+        print("Lower")
+    elif answer < number:
+        print("Higher")
