2b80c2d20436a9defdbd7249ee088682bdabc746
Jesper Pedersen <54931767+Jesp9025@users.noreply.github.com>
2019-10-12 21:19:56 +0200

    Update SubnettingGame.py

--- SubnettingGame/SubnettingGame.py
+++ SubnettingGame/SubnettingGame.py
@@ -7,6 +7,7 @@ print("Welcome to the subnetting game.\nHere you will get a question regarding s
 print("You will need to type the number of the answer you think is correct.")
 print("We prepared a little calculator if you need some help")
 print("#######################################################")
+
 #Basically if number is 1, it will ask question no.1, if number is 2, it will ask question no.2 etc.
 if number == 1:
     correctAns = 3
@@ -15,6 +16,7 @@ if number == 1:
         print("Correct.\nYou get 1 point.")
     else:
         print("Wrong. The correct answer is", correctAns, "\nYou lose 1 point.")
+
 elif number == 2:
     correctAns = 2
     answer = int(input("Enter the broadcast address for the network 10.184.0.0/16\n1: 10.184.255.252\n2: 10.184.255.255\n3: 10.184.255.224\n4: 10.184.255.240\nYour answer: "))
@@ -22,6 +24,7 @@ elif number == 2:
         print("Correct.\nYou get 1 point.")
     else:
         print("Wrong. The correct answer is", correctAns, "\nYou lose 1 point.")
+
 elif number == 3:
     correctAns = 2
     answer = int(input("What subnet mask would you use for the 172.29.0.0 network, such that you can get 2370 subnets and 10 hosts per subnet?\n1: 255.255.255.0\n2: 255.255.255.240\n3: 255.255.255.224\n4: 255.255.0.0\nYour answer: "))
@@ -29,6 +32,7 @@ elif number == 3:
         print("Correct.\nYou get 1 point.")
     else:
         print("Wrong. The correct answer is", correctAns, "\nYou lose 1 point.")
+
 elif number == 4:
     correctAns = 4
     answer = int(input("What is the Subnet Mask corresponding to a shorthand of /20?\n1: 255.255.255.0\n2: 255.255.224.0\n3: 255.255.0.0\n4: 255.255.240.0\Your answer: "))
@@ -36,6 +40,7 @@ elif number == 4:
         print("Correct.\nYou get 1 point.")
     else:
         print("Wrong. The correct answer is", correctAns, "\nYou lose 1 point.")
+
 elif number == 5:
     correctAns = 1
     answer = int(input("What is the Subnet Mask corresponding to a shorthand of /29?\n1: 255.255.255.248\n2: 255.255.255.224\n3: 255.255.255.240\n4: 255.255.255.0\nYour answer: "))
@@ -43,6 +48,7 @@ elif number == 5:
         print("Correct.\nYou get 1 point.")
     else:
         print("Wrong. The correct answer is", correctAns, "\nYou lose 1 point.")
+
 elif number == 6:
     correctAns = 1
     answer = int(input("What subnet mask would you use for the 172.28.0.0 network, such that you can get 40 subnets and 730 hosts per subnet?\1: 255.255.252.0\n2: 255.255.224.0\n3: 255.255.255.0\4: 255.255.240.0\nYour answer: "))
@@ -50,6 +56,7 @@ elif number == 6:
         print("Correct.\nYou get 1 point.")
     else:
         print("Wrong. The correct answer is", correctAns, "\nYou lose 1 point.")
+
 elif number == 7:
     correctAns = 2
     answer = int(input("Enter the first valid host on the network 172.17.0.0 255.255.0.0\n1: 172.17.0.0\n2: 172.17.0.1\n3: 172.17.0.16\n4: 172.17.0.128\nYour answer: "))
@@ -57,6 +64,7 @@ elif number == 7:
         print("Correct.\nYou get 1 point.")
     else:
         print("Wrong. The correct answer is", correctAns, "\nYou lose 1 point.")
+        
 elif number == 8:
     correctAns = 3
     answer = int(input("Enter the first valid host on the network 10.71.0.0/16\n1: 10.71.0.0\n2: 10.71.0.16\n3: 10.71.0.1\n4: 10.71.0.16\nYour answer: "))
