b987841a8108c325e35f418c03018b29fafaab35
Jesper Pedersen <54931767+Jesp9025@users.noreply.github.com>
2019-10-23 21:51:55 +0200

    Create temp.py
    Just a place to keep useful functions

---
+++ CoreGame/temp.py
@@ -0,0 +1,23 @@
+#Function to choose difficulty
+def difficulty():
+    global pointsToWin
+    while True:
+        choice = input("Please choose a difficulty..\na: Easy\nb: Veteran\nc: Global Elite\nYour choice: ").upper()
+        if choice == "A":
+            pointsToWin = 5
+            break
+        elif choice == "B":
+            pointsToWin = 10  
+            break
+        elif choice == "C":
+            pointsToWin = 15
+            break
+
+
+#https://stackoverflow.com/questions/2084508/clear-terminal-in-python
+#Function to clear the terminal         
+def clearTerminal():
+    if platform.system()=="Windows":
+        subprocess.Popen("cls", shell=True).communicate()
+    else: #Linux and Mac
+        print("\033c", end="")
