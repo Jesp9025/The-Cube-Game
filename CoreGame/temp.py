4cab2c85144fe8b3722078305b4a6ddf9b92e9f6
Jesper Pedersen <54931767+Jesp9025@users.noreply.github.com>
2019-10-26 16:04:49 +0200

    Update temp.py

--- CoreGame/temp.py
+++ CoreGame/temp.py
@@ -31,3 +31,15 @@ def countdown(n):
     n -= 1
     time.sleep(1)
   print("GO!")
+
+#Asks if player is ready to start or not
+def ready():
+    while True:
+        choice = input("Well then, are you ready to play the game? (type 'yes' or 'no'): ").upper()
+
+        if choice == "YES" or choice == "Y":
+            break
+        elif choice == "NO" or choice == "N":
+            sys.exit(print("Well goodbye then"))   
+        else:
+            print ("We just told you to enter 'Yes' or 'No' for crying out loud.")
