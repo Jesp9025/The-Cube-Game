a182af1f8735bb5975e1905e05e13b7881c054f6
Jesper Pedersen <54931767+Jesp9025@users.noreply.github.com>
2019-10-27 17:26:31 +0100

    Update MathGame.py

--- MathGame/MathGame.py
+++ MathGame/MathGame.py
@@ -16,8 +16,8 @@ def mathFunc():
     print ("Your question is",rand,operation,rand2) #tells the user what the equation is
     d=int(input ("What is your answer: ")) #asks the user for the awnser
     if d==maths: #checks if the input is the same as the awnser
-        print ("Correct, you may move to the next room") #output if the awnser is correct
+        print ("Correct\nThe dragon takes 50 damage") #output if the awnser is correct
         points = -50
     else:
-        print ("Incorrect. The actual answer is",maths) #output if the awnser is wrong, and tells the right awnser
+        print ("Incorrect. The actual answer is",maths, "\nThe dragon regenerates 15 health") #output if the awnser is wrong, and tells the right awnser
         points = 15
