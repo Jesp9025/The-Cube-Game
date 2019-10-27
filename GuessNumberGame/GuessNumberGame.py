d1986003edfe5a9853672bb2b53465757d7e3139
Jesper Pedersen <54931767+Jesp9025@users.noreply.github.com>
2019-10-27 17:10:48 +0100

    Update GuessNumberGame.py

--- GuessNumberGame/GuessNumberGame.py
+++ GuessNumberGame/GuessNumberGame.py
@@ -1,13 +1,12 @@
 import random
 
-#This is the welcome message
 def start():
     print("########################################")
-    print("Welcome to the Number-Guessing-Game\nYou have 3 attempts to guess the correct number.\nThe number is generated randomly from 1 to 10\n")
-
-#This is the game itself
+    print("Welcome to the Number-Guessing-Game\nYou have 3 attempts to guess the correct number.\nThe number is generated randomly from 1 to 10")
+    print("Winning the game will deal 120 damage to the dragon\nLosing the game will regenerate 25 of dragons health\n")
+    
 def game():
-    number = random.randint(1,10) #Change the numbers to increase/decrease difficulty
+    number = random.randint(1,10)
     attempts = 3
     global points #points is used to give/take points from the player when he finishes the game
     while True: #This will make the code run until it reaches a break
@@ -16,12 +15,12 @@ def game():
             if answer >= 1 and answer <= 10: #If answer is higher than or equal to 1, and less than or equal to 10, do the code
                 attempts -= 1
                 if answer == number: #If user guesses the correct number
-                    print("You guessed the correct number!\nYou get 1 point.")
-                    points = 1
+                    print("You guessed the correct number!\nThe dragon takes 120 damage")
+                    points = -120
                     break
                 elif attempts <= 0: #If user runs out of attempts
-                    print("You ran out of attempts.\nThe correct answer is", number, "\nYou lose 1 point.")
-                    points = -1
+                    print("You ran out of attempts.\nThe correct answer is", number, "\nThe dragon regenerates 25 health")
+                    points = 25
                     break
                 elif answer > number: #Tells the user to try a lower number
                     print("Try a lower number")
