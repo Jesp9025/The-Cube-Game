e87d7b8c79c1000f382037a77781e0b8ccbe2c7a
Jesper Pedersen <54931767+Jesp9025@users.noreply.github.com>
2019-11-15 09:58:26 +0100

    Update SimpleGUI.py

--- SimpleGUI.py
+++ SimpleGUI.py
@@ -1,24 +1,26 @@
-#Simple GUI from our Cube Game 
+#Simple GUI from our Cube Game
+#Edit of 'demo2' from Kaj
 import PySimpleGUI as sg
  
-sg.change_look_and_feel('GreenTan')
+sg.change_look_and_feel('Reddit')
  
 layout = [
     [sg.Text('The Cube Game', size=(
-        30, 1), justification='center', font=("Helvetica", 25), relief=sg.RELIEF_RIDGE, background_color='#C93515')],
-    [sg.Text('Welcome to the Cube Game.\nPlease select a difficulty',
-            background_color='#C93515')],
+        30, 1), justification='center', font=("Helvetica", 25), relief=sg.RELIEF_RIDGE)],
+    [sg.Text('Welcome to this mini-story text adventure game')],
+    [sg.Text('Please enter your name')],
+    [sg.InputText()],
+    [sg.Text('Please select a difficulty')],
     [sg.Frame(layout=[
-        [sg.Radio('Easy', "RADIO1", default=True, background_color='#808080'),
-         sg.Radio('Normal', "RADIO1", background_color='#808080'),
-         sg.Radio('Hard', "RADIO1", background_color='#808080')]], title='Difficulty',
+        [sg.Radio('Easy', "RADIO1", default=True),
+         sg.Radio('Normal', "RADIO1"),
+         sg.Radio('Hard', "RADIO1")]], title='Difficulty',
              title_color='black',
              relief=sg.RELIEF_SUNKEN,
-             tooltip='Use these to set difficulty',
-             background_color='#C93515')],
+             tooltip='Use these to set difficulty')],
     [sg.Submit(button_text='Start', tooltip='Press to start the game'), sg.Cancel()]]
- 
+
 window = sg.Window('The Cube Game', layout,
-    default_element_size=(40, 1), grab_anywhere=False, background_color='#C93515')
+    default_element_size=(40, 1), grab_anywhere=False)
  
 event, values = window.read()
